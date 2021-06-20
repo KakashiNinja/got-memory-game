aebd95167672e8305cf07ad0c36c1be5f9ae3976
DevChaudhary78 <devchaudhary.dc84.dc@gmail.com>
2021-06-20 17:59:30 +0530

    Basics done just need to render images for game

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div id='character-grid'>
+      <div>
+        <div id='img1'>
+          <img src='../assets/characters/dany.jpg' s alt='' />
+        </div>
+        <h3>Daenerys Targaryen</h3>
+      </div>
+      <div>
+        <div id='img2'></div>
+        <h3>Tyrion Lannister</h3>
+      </div>
+      <div>
+        <div id='img3'></div>
+        <h3>Jorah Mormont</h3>
+      </div>
+      <div>
+        <div id='img4'></div>
+        <h3>Arya Stark</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
